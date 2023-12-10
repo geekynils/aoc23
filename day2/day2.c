0d40059e6daf5f9c873a89295cf1712f8ec00e52
Nils Bruenggel <nils.bruenggel@roche.com>
2023-12-10 23:57:22 +0100

    Replace question with link

--- day2/day2.c
+++ day2/day2.c
@@ -1,9 +1,5 @@
 /*
-First question:
-
-Determine which games would have been possible if the bag had been loaded with
-only 12 red cubes, 13 green cubes, and 14 blue cubes. What is the sum of the 
-IDs of those games?
+Question: https://adventofcode.com/2023/day/2
 */
 
 #include <stdio.h>
